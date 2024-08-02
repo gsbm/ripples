81eed940b70f94f442a7b0387b75a533657d8d45
gsbm <31590269+gsbm@users.noreply.github.com>
2024-08-02 23:39:11 +0200

    Update rollup.config.js

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,18 +3,19 @@ import license from 'rollup-plugin-license'
 
 const version = require('../package.json').version
 const banner =
-`jQuery Ripples plugin v${version} / https://github.com/sirxemic/jquery.ripples
+`Ripples library v0.1.0 / https://github.com/gsbm/ripples
+Based on Jquery Ripples plugin / https://github.com/sirxemic/jquery.ripples
 MIT License
-@author sirxemic / https://sirxemic.com/`
+@original_author sirxemic / https://sirxemic.com/
+@author gsbm`
 
 export default {
   entry: path.join(__dirname, '../src/main.js'),
-  dest: path.join(__dirname, '../dist/jquery.ripples.js'),
+  dest: path.join(__dirname, '../dist/ripples.js'),
   format: 'umd',
   plugins: [
     license({
       banner
     })
-  ],
-  external: [ 'jquery' ]
+  ]
 }
